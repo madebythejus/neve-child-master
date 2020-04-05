d782258f34284fcd5c5aee6d817aef0985688f5e
Thejus <thejus@hellokaya.net>
2020-04-05 11:16:50 -0700

    updated resources block

--- blocks/resources/block.php
+++ blocks/resources/block.php
@@ -12,24 +12,26 @@
 				$slug       = strtolower( get_the_title()); 
 				$slug       = str_replace(" ", "-", $slug);
 				$slug       = preg_replace("/[-]+/i", "-", $slug); ?>
-
-				<!-- Putting in post title in a div with slug as ID -->
-				<div>
-					<h2 id="<?php echo $slug; ?>" class="resourceHeader"><?php echo the_title(); ?></h2>
+				<div class="categoryContainer">
+					<!-- Putting in post title in a div with slug as ID -->
+					<div class="categoryHeading">
+						<h3 id="<?php echo $slug; ?>" class="resourceHeader"><?php echo the_title(); ?></h3>
+					</div>
+					<ul class="categoryListItems">
+						<!-- working through the repeater field -->
+						<?php $fields = CFS()->get( 'resource_items' );
+							foreach ( $fields as $field ) 
+								{	
+									// Putting link and label in variables
+									$link = $field['resource_link'];
+									$label = $field['resource_label'];
+									// echoing a link with slug as class, link as href and label inside
+									echo "<li class=\"listItem\"><a class=\"" . $slug . "\" href=\"" . $link . "\">" . $label . "</a></li>";
+								
+								}    
+						?>
+					</ul>
 				</div>
-				<!-- working through the repeater field -->
-				<?php $fields = CFS()->get( 'resource_items' );
-					foreach ( $fields as $field ) 
-						{	
-							// Putting link and label in variables
-							$link = $field['resource_link'];
-							$label = $field['resource_label'];
-							// echoing a link with slug as class, link as href and label inside
-							echo "<a class=\"" . $slug . "\" href=\"" . $link . "\">" . $label . "</a>";
-							echo "<br>";
-						
-						}    
-				?>
 
 			<?php endwhile; ?>
 		<?php endif ?>
